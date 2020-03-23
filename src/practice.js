05086c8bb7862850656c4b3ee1bfe8b56a1f923a
Olivia Davis <olivia.davis593@gmail.com>
2020-03-23 12:43:50 -0500

    connected knex to a database

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,14 @@
-console.log('hello, practice')
+require('dotenv').config()
+//required knex
+const knex = require('knex')
+
+//created knex instance 
+//specifies where to connect to, username & password & which driver to use
+const knexInstance = knex({
+    client: 'pg',
+    //moved string into enviornment var => don't want connection details hardcoded 
+    //then we required & used the environmental var here (below & line: 1)
+    connection: process.env.DB_URL
+})
+
+console.log('knex and driver installed correctly');
